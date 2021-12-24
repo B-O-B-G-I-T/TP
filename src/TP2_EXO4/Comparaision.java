bdf62c2205647e327ba122b3b494f601ec312bfb
jean <forconijean@gmail.com>
2021-12-24 11:29:52 +0100

    vraiment du mal avec compare et compareTo

---
+++ src/TP2_EXO4/Comparaision.java
@@ -0,0 +1,46 @@
+package TP2_EXO4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Comparaision {
+
+    // static final Comparator<String> InverseComparator = new Comparator<String>() {
+    //     public int compare(String e1, String e2) {
+    //         return e2.compareTo(e1);
+    //     }
+    // };
+    // static final Comparator<String> militaireComparator = new Comparator<String>() {
+    //     public int compareTo(String e1){
+
+    //     }
+    //     public int compare(String e1, String e2) {
+    //         int comp = e1.compareTo(e2);
+    //         if (comp != 0)
+    //             return comp;
+    //         else
+    //             return e1.compareTo(e2);
+    //     }
+    // };
+
+    public static void main(String[] args) {
+        ComparatorMilitaire compMilitaire = new ComparatorMilitaire();
+        List<String> c = new ArrayList<String>();
+
+        for (String v : args) {
+            c.add(v);
+        }
+
+        System.out.println("Sans le trie " + c);
+        Collections.sort(c);
+        System.out.println("Trie lexical " + c);
+        Collections.sort(c, InverseComparator);
+        System.out.println("trie inverse " + c);
+        Collections.sort(c, compMilitaire);
+        System.out.println("trie militaire " + c);
+
+    }
+
+}
